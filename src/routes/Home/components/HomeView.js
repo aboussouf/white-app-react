6472dc5cdc2c38ea8fb97cf294c5310988fb02f3
aboussouf <ali.boussouf@yahoo.fr>
2018-05-03 11:18:02 +0100

    add whiteapp

---
+++ src/routes/Home/components/HomeView.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+import './HomeView.scss'
+
+export const HomeView = () => (
+  <div>
+    <h4>Welcome!</h4>
+    <img alt='This is a logo !' className='logo' src={logo} />
+  </div>
+)
+
+export default HomeView
---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+// We only need to import the modules necessary for initial render
+import CoreLayout from '../layouts/PageLayout/PageLayout'
+import Home from './Home'
+
+export const createRoutes = (store) => ({
+  path        : '/',
+  component   : CoreLayout,
+  indexRoute  : Home,
+  childRoutes : [
+  ]
+})
+
+export default createRoutes
